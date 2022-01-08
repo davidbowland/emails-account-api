ffe6ef86687bd4457f35fa91f76e80a993d3c245
David Bowland <dbowland1@gmail.com>
2022-01-08 03:14:58 -0600

    Simplify PATCH handler

--- src/handlers/patch-item.ts
+++ src/handlers/patch-item.ts
@@ -7,23 +7,18 @@ import { AccountPreference, APIGatewayEvent, APIGatewayProxyResult, PatchOperati
 import { extractJsonPatchFromEvent, getIdFromEvent } from '../utils/events'
 import { log, logErrorWithDefault } from '../utils/logging'
 
-const performPatch = (preference: AccountPreference, patchOperations: PatchOperation[]): Promise<AccountPreference> =>
-  Promise.resolve().then(
-    () => applyPatch(preference, patchOperations, throwOnInvalidJsonPatch, mutateObjectOnJsonPatch).newDocument
-  )
-
 const patchPreferenceById = (accountId: string, patchOperations: PatchOperation[]): Promise<APIGatewayProxyResult> =>
-  getDataByKey(accountId)
-    .then((preference: AccountPreference) =>
-      performPatch(preference, patchOperations)
-        .then((updatedPreference: AccountPreference) =>
-          setDataByKey(accountId, updatedPreference)
-            .then(() => ({ ...status.OK, body: JSON.stringify(updatedPreference) }))
-            .catch(logErrorWithDefault(status.INTERNAL_SERVER_ERROR))
-        )
-        .catch((err) => ({ ...status.BAD_REQUEST, body: JSON.stringify({ message: err }) }))
+  getDataByKey(accountId).then((preference: AccountPreference) =>
+    Promise.resolve(
+      applyPatch(preference, patchOperations, throwOnInvalidJsonPatch, mutateObjectOnJsonPatch).newDocument
     )
-    .catch(logErrorWithDefault(status.INTERNAL_SERVER_ERROR))
+      .then((updatedPreference: AccountPreference) =>
+        setDataByKey(accountId, updatedPreference)
+          .then(() => ({ ...status.OK, body: JSON.stringify(updatedPreference) }))
+          .catch(logErrorWithDefault(status.INTERNAL_SERVER_ERROR))
+      )
+      .catch((err) => ({ ...status.BAD_REQUEST, body: JSON.stringify({ message: err }) }))
+  )
 
 export const patchItemHandler = (event: APIGatewayEvent): Promise<APIGatewayProxyResult> =>
   log('Received event', { ...event, body: undefined })
