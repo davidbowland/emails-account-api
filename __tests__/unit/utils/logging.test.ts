95fc7e7b864f178bdae872dedc2f0e15bd0352ce
David Bowland <dbowland1@gmail.com>
2022-01-18 23:22:02 -0600

    Update logging tests

--- __tests__/unit/utils/logging.test.ts
+++ __tests__/unit/utils/logging.test.ts
@@ -1,13 +1,26 @@
 import { log, logError } from '@utils/logging'
 
 describe('logging', () => {
+  const consoleError = console.error
+  const consoleLog = console.log
+
+  beforeAll(() => {
+    console.error = jest.fn()
+    console.log = jest.fn()
+  })
+
+  afterAll(() => {
+    console.error = consoleError
+    console.log = consoleLog
+  })
+
   describe('log', () => {
     test.each(['Hello', 0, null, undefined, { a: 1, b: 2 }])(
       'expect logFunc to have been called with message',
-      (value) => {
+      async (value) => {
         const message = `Log message for value ${JSON.stringify(value)}`
 
-        log(message)
+        await log(message)
         expect(console.log).toHaveBeenCalledWith(message)
       }
     )
@@ -16,11 +29,11 @@ describe('logging', () => {
   describe('logError', () => {
     test.each(['Hello', 0, null, undefined, { a: 1, b: 2 }])(
       'expect logFunc to have been called with message',
-      (value) => {
+      async (value) => {
         const message = `Error message for value ${JSON.stringify(value)}`
         const error = new Error(message)
 
-        logError(error)
+        await logError(error)
         expect(console.error).toHaveBeenCalledWith(error)
       }
     )
--- jest.setup-test-env.js
+++ jest.setup-test-env.js
@@ -1,10 +1,3 @@
 // DynamoDB
 
 process.env.DYNAMODB_TABLE_NAME = 'account-table'
-
-// Console
-
-console.info = jest.fn()
-console.log = jest.fn()
-console.warn = jest.fn()
-console.error = jest.fn()
